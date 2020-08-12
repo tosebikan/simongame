3340e7f91059dbf70e3b13978eb3afa5fb109fc4
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-08-12 09:04:16 +0000

    update

--- game.js
+++ game.js
@@ -1,10 +1,21 @@
 var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
+var level = 0;
+var started = false;
+
+$(document).on("keypress", function() {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+  }
+  started = true;
+});
 
 function nextSequence() {
+  level += 1;
+  $("#level-title").text(`level ${level}`);
   var randomNumber = Math.round(Math.random() * 3);
-  // console.log(randomNumber);
 
   var randomChosenColor = buttonColors[randomNumber];
 
@@ -14,14 +25,25 @@ function nextSequence() {
     .fadeOut(250)
     .fadeIn(250);
 
-  var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
-  audio.play();
+  playSound(randomChosenColor);
 
   $(".btn").on("click", function(event) {
     var userChosenColor = event.target.id;
     userClickedPattern.push(userChosenColor);
     console.log(userClickedPattern);
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
   });
 }
 
-nextSequence();
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColor) {
+  $(`#${currentColor}`).addClass("pressed");
+  setTimeout(function() {
+    $(`#${currentColor}`).removeClass("pressed");
+  }, 100);
+}
