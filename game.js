178ac39c939374cd4e6ccdd8650739d4669c82ac
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-08-13 16:38:37 +0000

    update

--- game.js
+++ game.js
@@ -4,36 +4,62 @@ var userClickedPattern = [];
 var level = 0;
 var started = false;
 
-$(document).on("keypress", function() {
+$(document).keypress(function() {
   if (!started) {
     $("#level-title").text("Level " + level);
     nextSequence();
+    started = true;
   }
-  started = true;
 });
 
+$(".btn").click(function() {
+  var userChosenColor = $(this).attr("id");
+  userClickedPattern.push(userChosenColor);
+
+  playSound(userChosenColor);
+  animatePress(userChosenColor);
+  checkAnswer(userClickedPattern.length - 1);
+});
+
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    console.log("Success");
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function() {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    console.log("Wrong");
+    playSound("wrong");
+    $("body").addClass("game-over");
+    setTimeout(function() {
+      $("body").removeClass("game-over");
+    }, 200);
+    $("h1").text("Game Over");
+    $(document).keypress(function() {
+      level = 0;
+      nextSequence();
+    });
+  }
+}
+
 function nextSequence() {
-  level += 1;
+  userClickedPattern = [];
+  level++;
   $("#level-title").text(`level ${level}`);
-  var randomNumber = Math.round(Math.random() * 3);
+  var randomNumber = Math.floor(Math.random() * 4);
 
   var randomChosenColor = buttonColors[randomNumber];
 
   gamePattern.push(randomChosenColor);
+  console.log(gamePattern);
 
   $(`#${randomChosenColor}`)
     .fadeOut(250)
     .fadeIn(250);
 
   playSound(randomChosenColor);
-
-  $(".btn").on("click", function(event) {
-    var userChosenColor = event.target.id;
-    userClickedPattern.push(userChosenColor);
-    console.log(userClickedPattern);
-    playSound(userChosenColor);
-    animatePress(userChosenColor);
-  });
 }
 
 function playSound(name) {
